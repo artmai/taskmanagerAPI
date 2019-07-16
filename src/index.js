eed438cba696773da4e87f918a514efba9f86f20
artmai <arthurmaisonnier@gmail.com>
2019-07-16 09:06:29 +0200

    pushed to audit

--- src/index.js
+++ src/index.js
@@ -4,3 +4,9 @@ const port = process.env.PORT
 app.listen(port, () => {
   console.log('server is up on port ' + port)
 })
+
+
+// Starting Plaid integration
+const plaid = require('plaid');
+
+const plaidClient = new plaid.Client(client_id, secret, public_key, plaid.environments.sandbox, {version: '2018-05-22'});
